2669bc23e5aaccc4c4911683fd24b5121c22bb89
Lucy Ashour <lucy@servicecore.com>
2023-03-16 12:25:23 -0600

    feat: add fontawesome. Tweaks/minor progress.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IntroComponent } from './intro/intro.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CredentialsComponent } from './credentials/credentials.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    ProjectsComponent,
+    CredentialsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
